2138bb000569ebd5395c27cda6b3f7d48c898eb2
UnknownKing <unknownking40777@gmail.com>
2025-11-16 03:03:46 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,97 +1,15 @@
-plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.ksp)
-    alias(libs.plugins.kotlin.android)
-}
-
 android {
-    namespace = "xyz.wingio.logra"
-    compileSdk = 33
-
-    defaultConfig {
-        applicationId = "xyz.wingio.logra"
-        minSdk = 26
-        targetSdk = 33
-        versionCode = 1330
-        versionName = "ALPHA - 1.30"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    signingConfigs {
+        create("release") {
+            storeFile = file("keystore.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
     }
-
     buildTypes {
-        named("release") {
-            isMinifyEnabled = true
-            isShrinkResources = true
-            setProguardFiles(listOf(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"))
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
         }
     }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-        freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
-        freeCompilerArgs += "-Xcontext-receivers"
-    }
-
-    buildFeatures {
-        compose = true
-        buildConfig = true
-    }
-
-    androidComponents {
-        onVariants(selector().withBuildType("release")) {
-            it.packaging.resources.excludes.apply {
-                // Debug metadata
-                add("/**/*.version")
-                add("/kotlin-tooling-metadata.json")
-                // Kotlin debugging (https://github.com/Kotlin/kotlinx.coroutines/issues/2274)
-                add("/DebugProbesKt.bin")
-            }
-        }
-    }
-
-    packaging {
-        resources {
-            // Reflection symbol list (https://stackoverflow.com/a/41073782/13964629)
-            excludes += "/**/*.kotlin_builtins"
-        }
-    }
-
-    sourceSets {
-        applicationVariants.all {
-            getByName(name) {
-                kotlin.srcDir("build/generated/ksp/$name/kotlin")
-            }
-        }
-    }
-}
-
-configurations {
-    implementation {
-        exclude("org.jetbrains", "annotations")
-    }
-}
-
-dependencies {
-    implementation(libs.bundles.androidx.core)
-    implementation(libs.bundles.compose)
-    implementation(libs.bundles.accompanist)
-    implementation(libs.bundles.koin)
-    implementation(libs.bundles.kotlinx)
-    implementation(libs.bundles.shizuku)
-    implementation(libs.bundles.voyager)
-    implementation(libs.bundles.room)
-
-    implementation(libs.colorpicker)
-    implementation(libs.bottomsheetdialog)
-
-    ksp(libs.room.compiler)
 }
