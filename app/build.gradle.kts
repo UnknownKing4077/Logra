f3c4a919c4837b72d47ab7ab9d93298672764273
UnknownKing <unknownking40777@gmail.com>
2025-11-16 22:02:20 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,50 +1,5 @@
+// build.gradle.kts (project level)
 plugins {
-    id("com.android.application")
-    kotlin("android") version "1.9.22"  // Use latest stable version
-}
-
-android {
-    namespace = "xyz.wingio.logra"
-    compileSdk = 34
-    
-    defaultConfig {
-        applicationId = "xyz.wingio.logra"
-        minSdk = 26
-        targetSdk = 34
-        versionCode = 1330
-        versionName = "1.30"
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-
-    signingConfigs {
-        create("release") {
-            storeFile = file("keystore.jks")
-            storePassword = System.getenv("KEYSTORE_PASSWORD")
-            keyAlias = System.getenv("KEY_ALIAS")
-            keyPassword = System.getenv("KEY_PASSWORD")
-        }
-    }
-
-    buildTypes {
-        getByName("release") {
-            signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = false
-        }
-    }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")  // Add this
-    implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("dev.rikka.shizuku:api:13.1.5")
-    implementation("dev.rikka.shizuku:provider:13.1.5")
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
